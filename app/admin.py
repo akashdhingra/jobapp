0b8693030bc7cd74ca5d55e118e5af8b12fe3c8f
Akash Dhingra <akashdhingra1996@gmail.com>
2024-02-16 18:57:23 -0500

    Updated help text

--- app/admin.py
+++ app/admin.py
@@ -6,7 +6,7 @@ class JobAdmin(admin.ModelAdmin):
     list_display = ('__str__','title', 'salary', 'date')
     list_filter = ('date','salary','expiry')
     search_fields = ('title','description')
-    search_help_text = "Write in your query and hit enter"
+    search_help_text = "Write in your query and hit enter "
     # fields = (('title','description'),'expiry')
     # exclude = ('title',)
     fieldsets = (
