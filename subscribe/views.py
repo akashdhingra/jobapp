bca68ee38ca839d9fe657d49e4231a84ee3bb007
Akash Dhingra <akashdhingra1996@gmail.com>
2024-02-19 15:54:58 -0500

    model for subscribe

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,6 +1,14 @@
+import email
 from django.shortcuts import render
 
 # Create your views here.
 def subscribe(request):
-    context={}
+    email_error_empty=""
+    if request.POST:
+        email = request.POST['email']
+        print("POST REQUEST : ", email)
+        if email=="":
+            email_error_empty = "No email entered"
+
+    context={"email_error_empty":email_error_empty}
     return render(request,'subscribe/subscribe.html',context)
