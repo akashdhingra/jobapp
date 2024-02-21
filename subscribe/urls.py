382abc895a73562ceea5b1559a3cd1fbeeefc70c
Akash Dhingra <akashdhingra1996@gmail.com>
2024-02-21 13:12:48 -0500

    finish saving of data from forms

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('subscribe/', views.subscribe, name='subscribe')
+    path('subscribe/', views.subscribe, name='subscribe'),
+    path('thank_you/',views.thank_you, name='thank_you')
 ]
