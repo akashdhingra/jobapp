eba838f62b4face97cab24eb2da2044e7086a429
Akash Dhingra <akashdhingra1996@gmail.com>
2024-02-21 22:19:35 -0500

    customization in modelforms

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 
 from subscribe.models import Subscribe
+from django.utils.translation import gettext_lazy as _
 
 class SubscribeForm(forms.ModelForm):
     class Meta:
         model=Subscribe
         fields='__all__'
+        # exclude=('first_name',)
+        labels={
+            'first_name':_('Enter first name'),
+            'last_name':_('Enter last name'),
+            'email':_('Enter email'),
+        }
+        help_texts = {'first_name':_('Enter character\'s only')}
+        error_messages={
+            'first_name':{
+                'required':_('You cannot move forward without first name')
+            }
+        }
 
 
 # def validate_comma(value):
