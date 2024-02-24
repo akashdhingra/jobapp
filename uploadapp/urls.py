2fe773741a79166bb88331860febb6d812dd9b36
Akash Dhingra <akashdhingra1996@gmail.com>
2024-02-24 11:58:09 -0500

    saving and uploading image

--- uploadapp/urls.py
+++ uploadapp/urls.py
@@ -1,8 +1,9 @@
 
+from subscribe.views import thank_you
 from . import views
 from django.urls import path
 
-
 urlpatterns = [
-    path('image',views.upload_image,name='upload_image')
+    path('image',views.upload_image,name='upload_image'),
+    path('thank_you',views.thank_you,name='thank_you')
 ]
--- uploadapp/views.py
+++ uploadapp/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from uploadapp.forms import UploadForm
 
 # Create your views here.
 def upload_image(request):
-    form = UploadForm()
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid:
+            form.save()
+            return redirect(reverse('thank_you'))
+    else:
+        form = UploadForm()
     return render(request,'uploadapp/add_image.html',{'form':form})
+
+def thank_you(request):
+    context = {}
+    return render(request,'uploadapp/thankyou.html',context)
